3fc020165d1c5b1e6182737671d550e0226c2216
kadaYogesh <kadayogeswararao@gmail.com>
2026-09-13 12:52:13 +0530

    feat: implement frontend logout

--- frontend/food-delivery-ui/src/app/features/dashboard/dashboard.ts
+++ frontend/food-delivery-ui/src/app/features/dashboard/dashboard.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Auth } from '../../core/services/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.scss',
 })
 export class Dashboard {
+   private authService = inject(Auth);
+   private router = inject(Router); 
 
+   logout(): void {
+  this.authService.logout();
+  this.router.navigate(['/login']);
+}
 }
